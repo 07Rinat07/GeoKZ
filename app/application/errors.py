9937abb1c96dd929b001d42b82673cc73c1fc781
S.Rinat <102806560+07Rinat07@users.noreply.github.com>
2026-09-04 22:10:28 +0500

    Add demo correlation selection error

--- app/application/errors.py
+++ app/application/errors.py
@@ -8,3 +8,7 @@ class ResourceNotFoundError(ApplicationError):
 
 class CoordinateResolutionError(ApplicationError):
     """Координаты или исходная CRS не могут быть безопасно преобразованы."""
+
+
+class DemoCorrelationSelectionError(ApplicationError):
+    """Выбор скважин не соответствует безопасному demo workflow."""
