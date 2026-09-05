a4c66522e2052c09665d016b94e3786ed45e7e1b
S.Rinat <102806560+07Rinat07@users.noreply.github.com>
2026-09-05 07:16:18 +0500

    Register authentication routes

--- app/api/router.py
+++ app/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.routes import (
     about,
+    auth,
     core_dataset,
     correlation,
     entities,
@@ -26,6 +27,11 @@ api_router.include_router(
     about.router,
     prefix=f"{settings.api_prefix}/about",
     tags=["about"],
+)
+api_router.include_router(
+    auth.router,
+    prefix=f"{settings.api_prefix}/auth",
+    tags=["auth"],
 )
 api_router.include_router(
     help.router,
